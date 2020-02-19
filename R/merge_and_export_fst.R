6e6d48a74fb5a1f4250aaac9cdcf42ef142bb7cd
hsonne <hauke.sonnenberg@gmx.de>
2020-02-19 11:19:04 +0100

    Clean merge_and_export_fst.R
    - use grep(..., value = TRUE)
    - use kwb.utils::catAndRun()
    - loop through the different aggregation levels (DRY)

--- R/merge_and_export_fst.R
+++ R/merge_and_export_fst.R
@@ -1,4 +1,5 @@
 #' Helper function: group fst by pattern
+#' 
 #' @param fst_pattern pattern to search for in fst filename (default: "raw")
 #' @param time_pattern optional pattern to filter months to be imported (default: NULL),
 #' for using it do e.g. "2017-06|2017-07" or c("2017-06", "2017-07")
@@ -15,34 +16,30 @@ group_fst_by_pattern <- function(
   fst_dir = package_file("shiny/berlin_t/data/fst")
 )
 {
-  fst_file_paths <- list.files(
-    path = fst_dir,
-    pattern = fst_pattern,
-    recursive = TRUE,
-    full.names = TRUE
-  )
+  files <- list.files(fst_dir, fst_pattern, recursive = TRUE, full.names = TRUE)
 
-  if (!is.null(time_pattern)) {
+  if (! is.null(time_pattern)) {
 
-    if (length(time_pattern)>1) time_pattern <- to_pattern_or(time_pattern)
+    if (length(time_pattern) > 1) {
+      time_pattern <- to_pattern_or(time_pattern)
+    }
 
-    indices <- stringr::str_detect(fst_file_paths, time_pattern)
-    fst_file_paths <- fst_file_paths[indices]
+    files <- grep(time_pattern, files, value = TRUE)
   }
 
-  cat(sprintf(
-    "Importing the following fst files:\n%s\n",
-    paste(fst_file_paths, collapse = "\n")
-  ))
-
-  system.time(merge_dt <- data.table::rbindlist(lapply(
-    fst_file_paths, read_fst
-  )))
-
-  return(merge_dt)
+  kwb.utils::catAndRun(
+    messageText = sprintf(
+      "Importing the following fst files:\n%s\n",
+      paste(files, collapse = "\n")
+    ), 
+    expr = {
+      data.table::rbindlist(lapply(files, read_fst))
+    }
+  )
 }
 
 #' Helper function: merge and export fst files into main shiny data folder
+#' 
 #' @param time_pattern optional pattern to filter months to be imported (default: NULL),
 #' for using it do e.g. "2017-06|2017-07" or c("2017-06", "2017-07")
 #' @param compression compression for fst export (default: 100)
@@ -59,92 +56,30 @@ merge_and_export_fst <- function(
   export_dir = package_file("shiny/berlin_t/data")
 )
 {
-  if (!dir.exists(export_dir)) {
-    print(sprintf("Creating export path: %s", export_dir))
-    dir.create(export_dir, recursive = TRUE)
+  if (! dir.exists(export_dir)) {
+    
+    kwb.utils::catAndRun(
+      sprintf("Creating export path: %s", export_dir),
+      dir.create(export_dir, recursive = TRUE)
+    )
   }
+  
+  for (fst_pattern in c("raw", "10min", "hour", "day")) {
 
-  siteData_raw_list <- group_fst_by_pattern(
-    time_pattern = time_pattern,
-    fst_pattern = "raw",
-    fst_dir = import_dir
-  )
-
-  exp_raw <- sprintf("%s/siteData_raw_list.fst", export_dir)
-  cat(sprintf(
-    "Writing fst: %s (with compression %d)\n",
-    exp_raw,
-    compression
-  ))
-  system.time(fst::write.fst(
-    siteData_raw_list,
-    path = exp_raw,
-    compress = compression
-  ))
-
-  rm(siteData_raw_list)
-
-  system.time(
-    siteData_10min_list <- group_fst_by_pattern(
-      time_pattern = time_pattern,
-      fst_pattern = "10min",
-      fst_dir = import_dir
+    site_data_list <- kwb.utils::catAndRun(
+      paste("Grouping by", fst_pattern),
+      group_fst_by_pattern(
+        time_pattern = time_pattern,
+        fst_pattern = fst_pattern,
+        fst_dir = import_dir
+      )
     )
-  )
 
-  exp_10min <- sprintf("%s/siteData_10min_list.fst", export_dir)
-  cat(sprintf(
-    "Writing fst: %s (with compression %d)\n",
-    exp_10min,
-    compression
-  ))
-  fst::write.fst(
-    siteData_10min_list,
-    path = exp_10min,
-    compress = compression
-  )
-
-  rm(siteData_10min_list)
-
-  system.time(
-    siteData_hour_list <- group_fst_by_pattern(
-      time_pattern = time_pattern,
-      fst_pattern = "hour",
-      fst_dir = import_dir
+    file <- file.path(export_dir, sprintf("siteData_%s_list.fst", fst_pattern))
+    
+    kwb.utils::catAndRun(
+      sprintf("Writing fst: %s (with compression %d)\n", file, compression),
+      fst::write.fst(site_data_list, path = file, compress = compression)
     )
-  )
-
-  exp_hour <- sprintf("%s/siteData_hour_list.fst", export_dir)
-  cat(sprintf(
-    "Writing fst: %s (with compression %d)\n",
-    exp_hour,
-    compression
-  ))
-
-  fst::write.fst(
-    siteData_hour_list,
-    path = exp_hour,
-    compress = compression
-  )
-
-  system.time(
-    siteData_day_list <- group_fst_by_pattern(
-      time_pattern = time_pattern,
-      fst_pattern = "day",
-      fst_dir = import_dir
-    )
-  )
-
-  exp_day <- sprintf("%s/siteData_day_list.fst", export_dir)
-  cat(sprintf(
-    "Writing fst: %s (with compression %d)\n",
-    exp_day,
-    compression
-  ))
-
-  fst::write.fst(
-    siteData_day_list,
-    path = exp_day,
-    compress = compression
-  )
+  }
 }
