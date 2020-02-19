ace1d62eab007c317890182f23437160164b042e
hsonne <hauke.sonnenberg@gmx.de>
2020-02-19 07:48:12 +0100

    Clean group_datetime.R
    * use selectColumns(), catAndRun() from kwb.utils

--- R/group_datetime.R
+++ R/group_datetime.R
@@ -1,4 +1,5 @@
 #' Group DateTime by user defined period (year, month, day, hour, minute)
+#' 
 #' @param df a data frame as retrieved by import_data_haridwar()
 #' @param by an aggregation time step in seconds (default: 600 seconds) for intra-
 #' day aggregation or "day", "month" or "year" for longer time spans
@@ -16,12 +17,15 @@
 #' @importFrom kwb.utils stringList
 #' @export
 
-group_datetime <- function(df,
-                           by = 600,
-                           fun = "stats::median",
-                           col_datetime = "DateTime",
-                           col_datatype = "DataType",
-                           dbg = TRUE) {
+group_datetime <- function(
+  df, 
+  by = 600, 
+  fun = "stats::median", 
+  col_datetime = "DateTime", 
+  col_datatype = "DataType", 
+  dbg = TRUE
+)
+{
   if (is.character(by)) {
     by <- tolower(by)
   }
@@ -32,40 +36,55 @@ group_datetime <- function(df,
     day = "%Y-%m-%d 00:00:00"
   )
 
-  if ((!by %in% names(grp_list)) & !is.numeric(by)) {
-    msg <- sprintf(
-      "'%s' is no valid aggregation time step!\n Please select one of: %s for parameter 'by' or
-                 provide a temporal aggregation interval in seconds (e.g. 600 in case of 10 minute aggregation))",
-      by,
-      kwb.utils::stringList(names(grp_list))
+  if (! is.numeric(by) && ! by %in% names(grp_list)) {
+    
+    clean_stop(
+      sprintf("'%s' is no valid aggregation time step!\n", by),
+      "Please select one of: ", kwb.utils::stringList(names(grp_list)), 
+      " for parameter 'by' or provide a temporal aggregation interval in ", 
+      "seconds (e.g. 600 in case of 10 minute aggregation)."
     )
-    clean_stop(msg)
-  } else if ((by %in% names(grp_list)) & !is.numeric(by)) {
-    datetime_org <- df[, col_datetime]
-    tz_org <- base::check_tzones(datetime_org)
-    df[, col_datetime] <- fasttime::fastPOSIXct(
-      format(
-        df[, col_datetime],
-        format = grp_list[[by]]
-      ),
-      tz = tz_org,
-      required.components = 3L
-    )
-    if (by == "day") by <- "dai"
-    if (dbg) print(sprintf("Performing %sly temporal aggregation!", by))
-
-    df[, col_datatype] <- sprintf("%s (%sly %s)", df[, col_datatype], by, fun)
-  } else {
-    if (dbg) print(sprintf("Performing temporal aggregation for %d seconds time periods!", by))
-    df[, col_datetime] <- xts::align.time(df[, col_datetime], n = by)
-    df[, col_datatype] <- sprintf("%s (%d seconds %s)", df[, col_datatype], by, fun)
   }
 
+  # Extract the time column
+  timestamps <- kwb.utils::selectColumns(df, col_datetime)
+  
+  if (! is.numeric(by)) {
+    
+    timely <- if (by == "day") "daily" else paste0(by, "ly")
+    
+    text <- paste("Performing", timely, "temporal aggregation!")
+    
+    times <- fasttime::fastPOSIXct(
+      format(timestamps, format = grp_list[[by]]),
+      tz = base::check_tzones(timestamps),
+      required.components = 3L
+    )
+    
+    descriptions <- sprintf("%s %s", timely, fun)
+    
+  } else {
+    
+    text <- paste(
+      "Performing temporal aggregation for", by, "seconds time periods!"
+    )
+    
+    times <- xts::align.time(timestamps, n = by)
+    
+    descriptions <- sprintf("%d seconds %s", by, fun)
+  }
 
-  df <- df %>%
-    dplyr::group_by_(.dots = dplyr::setdiff(names(df), "ParameterValue")) %>%
-    dplyr::summarise_("ParameterValue" = sprintf("%s(ParameterValue)", fun)) %>%
-    as.data.frame()
+  # Extract the type column
+  types <- kwb.utils::selectColumns(df, col_datatype)
+  
+  # Overwrite original columns with new time objects and type labels
+  df[, col_datetime] <- times
+  df[, col_datatype] <- sprintf("%s (%s)", types, descriptions)
 
-  return(df)
+  kwb.utils::catAndRun(text, dbg = dbg, expr = {
+    df %>%
+      dplyr::group_by_(.dots = dplyr::setdiff(names(df), "ParameterValue")) %>%
+      dplyr::summarise_(ParameterValue = paste0(fun, "(ParameterValue)")) %>%
+      as.data.frame()
+  })
 }
