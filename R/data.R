12c01d40c6576b6ebc0bcad54b490b723fc8727f
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-06-02 23:06:27 +0200

    Fixes for Rcmdcheck

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Metadata: MBR 4.0 pilot plant Berlin
+#' 
+#' A dataset with the following columns:  ParameterCode_SiteCode, 
+#' ParameterCode, SiteCode, ParameterName_SiteName, ParameterName, 
+#' SiteName, ParameterUnit, Comment, Source, DataType
+#'
+#' @format A data frame with 78 rows and 10 variables
+#'
+"mbr4.0_metadata"
