b8c7d5f2b87aa58b051aaa00baf10e4bd998f19d
hsonne <hauke.sonnenberg@gmx.de>
2021-11-12 19:33:57 +0100

    Clean group_datetime.R

--- R/group_datetime.R
+++ R/group_datetime.R
@@ -17,23 +17,28 @@
 #' @importFrom kwb.utils stringList
 #' @export
 
-group_datetime <- function(df,
-                           by = 600,
-                           fun = "stats::median",
-                           col_datetime = "DateTime",
-                           col_datatype = "DataType",
-                           dbg = TRUE) {
+group_datetime <- function(
+  df,
+  by = 600,
+  fun = "stats::median",
+  col_datetime = "DateTime",
+  col_datatype = "DataType",
+  dbg = TRUE
+)
+{
   if (is.character(by)) {
     by <- tolower(by)
   }
-
+  
+  add_midnight <- function(x) paste(x, "00:00:00")  
+  
   grp_list <- list(
-    year = "%Y-01-01 00:00:00",
-    month = "%Y-%m-01 00:00:00",
-    day = "%Y-%m-%d 00:00:00"
+    year = add_midnight("%Y-01-01"),
+    month = add_midnight("%Y-%m-01"),
+    day = add_midnight("%Y-%m-%d")
   )
-
-  if (!is.numeric(by) && !by %in% names(grp_list)) {
+  
+  if (! is.numeric(by) && ! by %in% names(grp_list)) {
     clean_stop(
       sprintf("'%s' is no valid aggregation time step!\n", by),
       "Please select one of: ", kwb.utils::stringList(names(grp_list)),
@@ -41,47 +46,53 @@ group_datetime <- function(df,
       "seconds (e.g. 600 in case of 10 minute aggregation)."
     )
   }
-
+  
   # Extract the time column
   timestamps <- kwb.utils::selectColumns(df, col_datetime)
-
-  if (!is.numeric(by)) {
+  
+  if (! is.numeric(by)) {
+    
     timely <- if (by == "day") "daily" else paste0(by, "ly")
-
+    
     text <- paste("Performing", timely, "temporal aggregation!")
-
+    
     times <- fasttime::fastPOSIXct(
       format(timestamps, format = grp_list[[by]]),
       tz = base::check_tzones(timestamps),
       required.components = 3L
     )
-
+    
     descriptions <- sprintf("%s %s", timely, fun)
+    
   } else {
+    
     text <- paste(
       "Performing temporal aggregation for", by, "seconds time periods!"
     )
-
+    
     times <- xts::align.time(timestamps, n = by) - by / 2
-
+    
     descriptions <- sprintf("%d seconds %s", by, fun)
   }
-
+  
   # Extract the type column
   types <- kwb.utils::selectColumns(df, col_datatype)
-
+  
   # Overwrite original columns with new time objects and type labels
-  df[, col_datetime] <- times
-  df[, col_datatype] <- sprintf("%s (%s)", types, descriptions)
-
+  df[[col_datetime]] <- times
+  df[[col_datatype]] <- sprintf("%s (%s)", types, descriptions)
+  
   kwb.utils::catAndRun(text, dbg = dbg, expr = {
     df %>%
-      dplyr::group_by(dplyr::across(.cols = dplyr::setdiff(names(df), 
-                                                           "ParameterValue"))
-                      ) %>%
-      dplyr::summarise(ParameterValue = eval(parse(text = paste0(fun, 
-                                                                 "(.data$ParameterValue)"))),
-                       .groups = "keep") %>%
+      dplyr::group_by(
+        dplyr::across(.cols = dplyr::setdiff(names(df), "ParameterValue"))
+      ) %>%
+      dplyr::summarise(
+        ParameterValue = eval(parse(
+          text = paste0(fun, "(.data$ParameterValue)")
+        )),
+        .groups = "keep"
+      ) %>%
       as.data.frame()
   })
 }
