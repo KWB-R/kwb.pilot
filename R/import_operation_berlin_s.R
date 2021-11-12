dff7f194f04bb02d0036d574e6706283d1f67079
hsonne <hauke.sonnenberg@gmx.de>
2021-11-12 20:35:46 +0100

    Clean import_operation_berlin_s.R

--- R/import_operation_berlin_s.R
+++ R/import_operation_berlin_s.R
@@ -4,29 +4,30 @@
 #' for meta file creation
 #' @return data.frame with meta data file structure
 #' @importFrom data.table fread
+#' @importFrom kwb.utils noFactorDataFrame
 #' @importFrom tidyr separate
 #' @export
-create_wedeco_metafile <- function(raw_data_file) {
+create_wedeco_metafile <- function(raw_data_file)
+{
   ozone <- kwb.utils::catAndRun(
     paste("Importing raw data file:", raw_data_file),
     data.table::fread(input = raw_data_file, header = TRUE, sep = ";", skip = 2)
   )
-
-  indices_names <- seq(from = 1, by = 2, to = ncol(ozone) - 1)
+  
+  indices_names <- seq(from = 1L, by = 2L, to = ncol(ozone) - 1L)
   indices_units <- indices_names + 1L
-
+  
   na_vector <- rep(NA, length(indices_names))
   units <- names(ozone)[indices_units]
-
-  meta_file <- data.frame(
+  
+  meta_file <- kwb.utils::noFactorDataFrame(
     ParameterCode = na_vector,
     ParameterName = na_vector,
     SiteCode = na_vector,
     SiteName = na_vector,
     ParameterName_SiteName = names(ozone)[indices_names],
     ParameterUnitOrg = units,
-    ParameterUnit = units,
-    stringsAsFactors = FALSE
+    ParameterUnit = units
   ) %>%
     tidyr::separate(
       col = "ParameterName_SiteName",
@@ -34,9 +35,9 @@ create_wedeco_metafile <- function(raw_data_file) {
       sep = " - ",
       remove = FALSE
     )
-
+  
   meta_file$ProzessID <- as.numeric(meta_file$ProzessID)
-
+  
   meta_file$ParameterUnit <- kwb.utils::multiSubstitute(
     strings = meta_file$ParameterUnit,
     replacements = list(
@@ -44,7 +45,7 @@ create_wedeco_metafile <- function(raw_data_file) {
       "V.*" = ""
     )
   )
-
+  
   meta_file
 }
 
@@ -59,9 +60,12 @@ create_wedeco_metafile <- function(raw_data_file) {
 #' @importFrom lubridate parse_date_time2
 #' @importFrom data.table rbindlist
 #' @export
-read_wedeco_data <- function(raw_data_dir = shiny_file("berlin_s/data/operation"),
-                             raw_data_files = NULL,
-                             meta_file_path = shiny_file("berlin_s/data/parameter_site_metadata.csv")) {
+read_wedeco_data <- function(
+  raw_data_dir = shiny_file("berlin_s/data/operation"),
+  raw_data_files = NULL,
+  meta_file_path = shiny_file("berlin_s/data/parameter_site_metadata.csv")
+)
+{
   meta_data <- read.csv(meta_file_path, stringsAsFactors = FALSE) %>%
     dplyr::select_(
       "ProzessID",
@@ -72,50 +76,48 @@ read_wedeco_data <- function(raw_data_dir = shiny_file("berlin_s/data/operation"
       "SiteName",
       "ZeroOne"
     )
-
+  
   meta_data$ParameterLabel <- sprintf(
     "%s (%s)",
     meta_data$ParameterName,
     meta_data$ParameterUnit
   )
-
+  
   files_to_import <- if (is.null(raw_data_files)) {
     list_full_csv_files(raw_data_dir)
   } else {
     raw_data_files
   }
-
-  raw_list <- lapply(
-    files_to_import,
-    FUN = function(pathfile) {
+  
+  raw_list <- lapply(files_to_import, function(pathfile) {
+    
       print(paste("Importing raw data file:", pathfile))
-
+      
       ozone <- data.table::fread(
-        pathfile,
-        header = TRUE, sep = ";", dec = ",", fill = TRUE, skip = 2
+        pathfile, header = TRUE, sep = ";", dec = ",", fill = TRUE, skip = 2
       )
-
-      indices_names <- seq(from = 1, by = 2, to = ncol(ozone) - 1)
+      
+      indices_names <- seq(from = 1L, by = 2L, to = ncol(ozone) - 1L)
       indices_units <- indices_names + 1L
-
-      process_ids <- stringr::str_sub(names(ozone)[indices_names], 6, 11)
-
+      
+      process_ids <- stringr::str_sub(names(ozone)[indices_names], 6L, 11L)
+      
       names(ozone)[indices_units] <- process_ids
-
-      ozone <- ozone[, c(1, indices_units), with = FALSE]
-
-      names(ozone)[1] <- "DateTime"
-
+      
+      ozone <- ozone[, c(1L, indices_units), with = FALSE]
+      
+      names(ozone)[1L] <- "DateTime"
+      
       ozone$DateTime <- lubridate::parse_date_time2(
         ozone$DateTime,
         orders = "d!.m!*.y!* H!:M!:S!",
         tz = "CET"
       )
-
+      
       columns <- c("DateTime", meta_data$ProzessID[meta_data$ZeroOne == 1])
-
+      
       relevant_paras <- names(ozone)[names(ozone) %in% columns]
-
+      
       ozone[, relevant_paras] %>%
         tidyr::gather_(
           key_col = "ProzessID",
@@ -125,18 +127,18 @@ read_wedeco_data <- function(raw_data_dir = shiny_file("berlin_s/data/operation"
         dplyr::mutate_(ProzessID = "as.numeric(ProzessID)")
     }
   )
-
+  
   meta_data <- meta_data %>%
     select_(.dots = "-ZeroOne")
-
+  
   df_tidy <- data.table::rbindlist(l = raw_list, use.names = TRUE) %>%
     dplyr::left_join(y = meta_data) %>%
     as.data.frame()
-
+  
   df_tidy$Source <- "online"
-
+  
   df_tidy$SiteName[kwb.utils::isNaOrEmpty(df_tidy$SiteName)] <- "General"
-
+  
   df_tidy
 }
 
@@ -153,19 +155,22 @@ read_wedeco_data <- function(raw_data_dir = shiny_file("berlin_s/data/operation"
 #' added data points in case of existing fst file was updated with new operational
 #' data
 #' @export
-import_data_berlin_s <- function(raw_data_dir = shiny_file("berlin_s/data/operation"),
-                                 raw_data_files = NULL,
-                                 meta_file_path = shiny_file("berlin_s/data/parameter_site_metadata.csv")) {
+import_data_berlin_s <- function(
+  raw_data_dir = shiny_file("berlin_s/data/operation"),
+  raw_data_files = NULL,
+  meta_file_path = shiny_file("berlin_s/data/parameter_site_metadata.csv")
+)
+{
   df <- read_wedeco_data(raw_data_dir, raw_data_files, meta_file_path)
-
+  
   df$DataType <- "raw"
-
+  
   df$SiteName_ParaName_Unit <- sprintf(
     "%s: %s (%s)", df$SiteName, df$ParameterName, df$ParameterUnit
   )
-
+  
   ### Remove duplicates if any exist
-  remove_duplicates(
-    df = df, col_names = c("DateTime", "ParameterCode", "SiteCode")
-  )
+  remove_duplicates(df = df, col_names = c(
+    "DateTime", "ParameterCode", "SiteCode"
+  ))
 }
