2b8a85e09e008ce41a1b150b85fd77b3ba10010b
mrustl <michael.rustler@kompetenz-wasser.de>
2021-11-17 08:23:23 +0100

    ultimate: current import status
    to do: check problematic tsv paths (line13) leading wide format conversion problem

--- R/.influxdb.R
+++ R/.influxdb.R
@@ -1,3 +1,4 @@
+library(kwb.pilot)
 paths_list <- list(influx_url = Sys.getenv("ULTIMATE_INFLUXDB_URL"),
                    influx_token = Sys.getenv("ULTIMATE_INFLUXDB_TOKEN"),
                    influx_org = Sys.getenv("ULTIMATE_INFLUXDB_ORG")
@@ -9,14 +10,13 @@ tsv_paths <- list.files("C:/kwb/projects/ultimate/raw_data_pilots/Pilot_B/data",
                         full.names = TRUE,
                         pattern = "xls$")
 
-
-pilot_b <- kwb.pilot::read_pentair_data(#raw_data_dir = "C:/kwb/projects/ultimate/raw_data_pilots/Pilot_B/",
-  raw_data_files = tsv_paths[51:100],
+tsv_paths[c(54,60,83,85,89)]
+#sapply(tsv_paths[c(51:53, 55:59)], function(file) { 
+#message(file)
+tmp_wide <- kwb.pilot::read_pentair_data(#raw_data_dir = "C:/kwb/projects/ultimate/raw_data_pilots/Pilot_B/",
+  raw_data_files = tsv_paths[c(80:82,84,86:88,90:100)], #file,
   meta_file_path = "") %>% 
-  dplyr::select(tidyselect::all_of(c("DateTime", "ParameterCode", "ParameterValue"))
-  )
-
-tmp_wide <- pilot_b %>% 
+  dplyr::select(tidyselect::all_of(c("DateTime", "ParameterCode", "ParameterValue"))) %>% 
   dplyr::filter(!is.na(ParameterValue),
                 !is.infinite(ParameterValue)) %>%
   tidyr::pivot_wider(names_from = "ParameterCode",
@@ -50,65 +50,67 @@ tmp_long <- tmp_long %>%
 
 
 +### R Client 
-
-remotes::install_github("influxdata/influxdb-client-r")
-
-client <- influxdbclient::InfluxDBClient$new(url = paths$influx_url,
-                                             token = paths$influx_token, 
-                                             org = paths$influx_org,
-                                             retryOptions = TRUE)
-
-# Ready status
-ready <- client$ready()
-
+  
+  #remotes::install_github("influxdata/influxdb-client-r")
+  
+  client <- influxdbclient::InfluxDBClient$new(url = paths$influx_url,
+                                               token = paths$influx_token,
+                                               org = paths$influx_org,
+                                               retryOptions = TRUE)
+  
+  # Ready status
+  #ready <- client$ready()
+  
 # Health info
 
-client$health() 
+#client$health() 
 
-sapply(fieldnames_with_changing_data, function(field_col) {
-
-tmp_dat <- tmp_long %>%  
-  dplyr::filter(ParameterCode == field_col) %>% 
-  tidyr::pivot_wider(names_from = "ParameterCode", 
-                     values_from = "ParameterValue") %>% 
-  as.data.frame()
-
-batch_size <- 5000
-ids <- seq(ceiling(nrow(tmp_dat)/batch_size))
-requests <- tibble::tibble(id = ids,
-                           idx_start = 1+(request_id-1)*batch_size,
-                           idx_end = request_id*batch_size
-                           )
-requests$idx_end[nrow(requests)] <- nrow(tmp_dat)
-sapply(ids, function(id) {
-  
-tmp_dat_split <-  tmp_dat[requests$idx_start[id]:requests$idx_end[id], ]
-
-msg_txt <- sprintf("'%s' (%d/%d), write request %d/%d (%s - %s, data points: %d, temporal resolution (avg): %ds) to InfluxDB",
-                   field_col,
-                   which(fieldnames_with_changing_data == field_col), 
-                   length(fieldnames_with_changing_data),
-                   id, 
-                   length(ids),
-                   min(tmp_dat_split$date_time),
-                   max(tmp_dat_split$date_time),
-                   nrow(tmp_dat_split),
-                   round(as.numeric(difftime(max(tmp_dat_split$date_time),
-                                             min(tmp_dat_split$date_time),
-                                             units = "secs"))/nrow(tmp_dat_split),
-                         0)
-                   )
-kwb.utils::catAndRun(messageText = msg_txt,expr = {
-client$write(tmp_dat_split, 
-             bucket = "ultimate", 
-             precision = "s",
-             measurementCol = "site_code",
-             tagCols = NULL, #"site_code",
-             fieldCols = field_col,
-             timeCol = "date_time")
-})
-}
-)
+system.time(expr = {
+  sapply(fieldnames_with_changing_data, function(field_col) {
+    
+    tmp_dat <- tmp_long %>%  
+      dplyr::filter(ParameterCode == field_col) %>% 
+      tidyr::pivot_wider(names_from = "ParameterCode", 
+                         values_from = "ParameterValue") %>% 
+      as.data.frame()
+    
+    batch_size <- 5000
+    ids <- seq(ceiling(nrow(tmp_dat)/batch_size))
+    requests <- tibble::tibble(id = ids,
+                               idx_start = 1+(ids-1)*batch_size,
+                               idx_end = ids*batch_size
+    )
+    requests$idx_end[nrow(requests)] <- nrow(tmp_dat)
+    sapply(ids, function(id) {
+      
+      tmp_dat_split <-  tmp_dat[requests$idx_start[id]:requests$idx_end[id], ]
+      
+      msg_txt <- sprintf("'%s' (%d/%d), write request %d/%d (%s - %s, data points: %d, temporal resolution (avg): %ds) to InfluxDB",
+                         field_col,
+                         which(fieldnames_with_changing_data == field_col), 
+                         length(fieldnames_with_changing_data),
+                         id, 
+                         length(ids),
+                         min(tmp_dat_split$date_time),
+                         max(tmp_dat_split$date_time),
+                         nrow(tmp_dat_split),
+                         round(as.numeric(difftime(max(tmp_dat_split$date_time),
+                                                   min(tmp_dat_split$date_time),
+                                                   units = "secs"))/nrow(tmp_dat_split),
+                               0)
+      )
+      kwb.utils::catAndRun(messageText = msg_txt,expr = {
+        client$write(tmp_dat_split, 
+                     bucket = "ultimate", 
+                     precision = "s",
+                     measurementCol = "site_code",
+                     tagCols = NULL, #"site_code",
+                     fieldCols = field_col,
+                     timeCol = "date_time")
+      })
+    }
+    )
+  })
 })
 
 
